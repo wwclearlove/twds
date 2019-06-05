fb7ca978ba459c660f7f069add6881e67af153a4
Rockly97 <38335896+Rockly97@users.noreply.github.com>
2019-06-05 23:06:12 +0800

    fcx

--- app/src/main/java/cdictv/twds/activity/MainActivity.java
+++ app/src/main/java/cdictv/twds/activity/MainActivity.java
@@ -1,18 +1,25 @@
 package cdictv.twds.activity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,6 +43,12 @@ public class MainActivity extends BaseActivity {
     private TextView id;
     private TextView djs;
     private WebView webview;
+    private TextView xlPopip;
+
+
+    private Context mContext;
+
+
     private int time=180;
     private ProgressDialog progressDialog;
     String uri;
@@ -68,9 +81,16 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = MainActivity.this;
         initView();
         initWeb();
         initlistener();
+        xlPopip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //initPopWindow(v);
+            }
+        });
     }
 
     private void initlistener() {
@@ -206,6 +226,7 @@ public class MainActivity extends BaseActivity {
         djs = (TextView) findViewById(R.id.djs);
         webview = (WebView) findViewById(R.id.webview);
         mAndroidID = DeviceUtils.getAndroidID(MainActivity.this);
+        xlPopip = (TextView) findViewById(R.id.xl_popip);
     }
 
     //-----显示ProgressDialog
@@ -239,4 +260,111 @@ public class MainActivity extends BaseActivity {
         return m.matches();
     }
 
+    
+
+
+
+
+    //点击下拉列表
+//    private void initPopWindow(View v) {
+//        View view = LayoutInflater.from(mContext).inflate(R.layout.item_popip, null, false);
+//        final TextView shouye = (TextView) view.findViewById(R.id.web_index);
+//        final TextView sys = (TextView) view.findViewById(R.id.web_sys);
+//        final TextView yx = (TextView) view.findViewById(R.id.web_yx);
+//        final TextView kq = (TextView) view.findViewById(R.id.web_kq);
+//        final TextView xx = (TextView) view.findViewById(R.id.web_xx);
+//        final TextView bz = (TextView) view.findViewById(R.id.web_bz);
+//        final TextView ws = (TextView) view.findViewById(R.id.web_ws);
+//        final TextView syaq = (TextView) view.findViewById(R.id.web_syaq);
+//        final TextView news = (TextView) view.findViewById(R.id.web_news);
+//        final TextView ss = (TextView) view.findViewById(R.id.web_ss);
+//        //1.构造一个PopupWindow，参数依次是加载的View，宽高
+//        final PopupWindow popWindow = new PopupWindow(view,
+//                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+//
+//        popWindow.setAnimationStyle(R.anim.anim_pop);  //设置加载动画
+//
+//        //这些为了点击非PopupWindow区域，PopupWindow会消失的，如果没有下面的
+//        //代码的话，你会发现，当你把PopupWindow显示出来了，无论你按多少次后退键
+//        //PopupWindow并不会关闭，而且退不出程序，加上下述代码可以解决这个问题
+//        popWindow.setTouchable(true);
+//        popWindow.setTouchInterceptor(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                return false;
+//                // 这里如果返回true的话，touch事件将被拦截
+//                // 拦截后 PopupWindow的onTouchEvent不被调用，这样点击外部区域无法dismiss
+//            }
+//        });
+//        popWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#B0000000")));    //要为popWindow设置一个背景才有效
+//
+//        //设置popupWindow显示的位置，参数依次是参照View，x轴的偏移量，y轴的偏移量
+//        popWindow.showAsDropDown(v, -10, 10);
+//
+//        //设置popupWindow里的按钮的事件
+//        shouye.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, shouye.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        sys.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, sys.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        yx.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        kq.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, kq.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        xx.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        bz.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        ws.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        syaq.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        news.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        ss.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, yx.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//    }
 }
+
